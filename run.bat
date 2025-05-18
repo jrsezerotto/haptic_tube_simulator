e251b8d27436fed74d9f16a01745d0cd4ee04168
jrsezerotto <junior_sezerotto@hotmail.com>
2025-05-18 10:21:31 -0300

    Updated bat script so that it fails in the case there is any error

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
+@echo off
+setlocal
 set "PATH=%PATH%;C:\msys64\mingw64\bin"
-cmake -B build -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="%~dp0vcpkg\scripts\buildsystems\vcpkg.cmake"
-cmake --build build
-.\build\torus_example.exe
+
+cmake -B build -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="%~dp0vcpkg\scripts\buildsystems\vcpkg.cmake" || exit /b 1
+cmake --build build || exit /b 1
+.\build\torus_example.exe || exit /b 1
